6a891da67072326134980196e938567d99052bba
Maricruz <maricruzpinlo@gmail.com>
2022-01-29 16:58:38 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  title : string = 'Contador App';
+  numero:number = 5;
+
+  /*sumar() {
+    this.numero += 1;
+  }
+
+  restar(){
+    this.numero -=1;
+  }*/
+
+
+   base: number =5;
+
+  acumular(base : number){
+    this.numero += base ;
+  }
 }
